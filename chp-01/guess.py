586a07ba6baaec834d17b9c365d00c7b1c9470fb
Victor deMatos <126367176+vvrmatos@users.noreply.github.com>
2024-05-31 00:50:12 -0300

    Create guess.py

---
+++ chp-01/guess.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Greetings
+print("Welcome!")
+
+# Data handling, conversion/casting
+g = input("Guess the number: ")
+guess = int(g)
+
+# Flow control, data verification
+if guess == 5:
+  print("You win!")
+else:
+  print("You lose!")
